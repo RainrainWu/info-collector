0f418c7333dc7b787229ff346408d24f9a73a3e8
RainrainWu <s0958334772@gmail.com>
2019-07-02 03:56:28 +0000

    Using webhook.

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,14 @@ import (
 
 func main() {
 
-	bot := tgbot.Tgbot{TunaBatch:2, BnextBatch:4, MeetBatch:3}
-	bot.AuthBot()
-	bot.Reply()
-
+	http.HandleFunc("/", bot)
 	port := os.Getenv("PORT")
 	http.ListenAndServe(":"+port, nil)
 }
 
+func bot (w http.ResponseWriter, r *http.Request) {
+
+	bot := tgbot.Tgbot{TunaBatch:2, BnextBatch:4, MeetBatch:3}
+        bot.AuthBot()
+        bot.Reply()
+}
