b78c1d050a92c20c6f19fec0e977fab818dce2ae
RainrainWu <s0958334772@gmail.com>
2019-07-02 05:44:15 +0000

    Update webhook.

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,23 @@ package main
 import (
 	"net/http"
 	"os"
+	"fmt"
 
 	"info-collector/pkg/tgbot"
 )
 
 func main() {
 
-	http.HandleFunc("/", bot)
-	port := os.Getenv("PORT")
-	http.ListenAndServe(":"+port, nil)
-}
-
-func bot (w http.ResponseWriter, r *http.Request) {
-
 	bot := tgbot.Tgbot{TunaBatch:2, BnextBatch:4, MeetBatch:3}
         bot.AuthBot()
         bot.Reply()
+
+	updates := bot.Bot.ListenForWebhook("/" + bot.Bot.Token)
+	port := os.Getenv("PORT")
+	http.ListenAndServe(":"+port, nil)
+
+	for update := range updates {
+
+		fmt.Println("%+v\n", update)
+	}
 }
